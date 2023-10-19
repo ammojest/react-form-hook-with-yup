3071d217519df49ba1a0560b7c725953486cd4c2
tom <tom.rhys.james@gmail.com>
2023-10-19 21:48:21 +0100

    changes form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,59 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form className="flex flex-col w-1/2 p-2 rounded-sm border-2 bg-white border-black">
-        <div className="flex justify-between">
-          <label htmlFor="first-name">First Name</label>
-          <input name="first-name" type="text" placeholder="first name" />
+      <form className="w-full max-w-sm">
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label
+              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              htmlFor="inline-full-name"
+            >
+              Full Name
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-full-name"
+              type="text"
+              defaultValue="Jane Doe"
+            />
+          </div>
         </div>
-        <div className="flex justify-between">
-          <label htmlFor="surname">Surname</label>
-          <input name="surname" type="text" placeholder="surname" />
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label
+              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              htmlFor="inline-password"
+            >
+              Password
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-password"
+              type="password"
+            />
+          </div>
         </div>
-        <div className="flex justify-between">
-          <label htmlFor="email">Email</label>
-          <input name="email" type="email" placeholder="email" />
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3"></div>
+          <label className="md:w-2/3 block text-gray-500 font-bold">
+            <input className="mr-2 leading-tight" type="checkbox" />
+            <span className="text-sm">Send me your newsletter!</span>
+          </label>
         </div>
-        <div className="flex justify-end">
-          <button className="bg-blue-600 text-white p-2 mt-5 rounded-md">
-            submit
-          </button>
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3">
+            <button
+              className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="button"
+            >
+              Sign Up
+            </button>
+          </div>
         </div>
       </form>
     </main>
